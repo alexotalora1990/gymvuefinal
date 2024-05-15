330f11b7848df1efa838cf92ab904b48f9eda8a9
alexotalora1990 <alexotaloracorredor@gmail.com>
2024-05-15 12:11:26 -0500

    actualizacion menu lateral app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <q-layout view="hHh lpR fFf">
 
-    <q-header elevated class="bg-primary text-white" height-hint="98">
+    <q-header class="bg-primary text-white">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-
         <q-toolbar-title>
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
-        Gym
+          Title
         </q-toolbar-title>
-      </q-toolbar>
 
-      <q-tabs align="left">
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="rightDrawerOpen" side="right" overlay behavior="desktop">
+      <q-tabs vertical>
         <q-route-tab to="/" label="inicio" />
         <q-route-tab to="/productos" label="productos" />
         <q-route-tab to="/usuarios" label="usuarios" />
@@ -26,17 +28,13 @@
         <q-route-tab to="/planes" label="planes" />
         <q-route-tab to="/ventas" label="ventas" />
       </q-tabs>
-    </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" side="left" overlay behavior="mobile" bordered>
-      <!-- drawer content -->
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
 
-    <q-footer elevated class="bg-grey-8 text-white">
+    <q-footer class="bg-grey-8 text-white">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
@@ -55,12 +53,12 @@ import { ref } from 'vue'
 
 export default {
   setup () {
-    const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
 
     return {
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
       }
     }
   }
